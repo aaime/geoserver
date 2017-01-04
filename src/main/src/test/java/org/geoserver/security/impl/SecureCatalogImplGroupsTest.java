d27b5af773272d3886bb30113ad8256d17549a65
Andrea Aime <andrea.aime@gmail.com>
2017-01-04 15:02:17 +0100

    More tests

--- src/main/src/test/java/org/geoserver/security/impl/SecureCatalogImplGroupsTest.java
+++ src/main/src/test/java/org/geoserver/security/impl/SecureCatalogImplGroupsTest.java
@@ -4,6 +4,7 @@
  */
 package org.geoserver.security.impl;
 
+import static org.hamcrest.Matchers.contains;
 import static org.junit.Assert.*;
 
 import java.io.StringReader;
@@ -14,6 +15,7 @@ import java.util.stream.Stream;
 
 import org.geoserver.catalog.LayerGroupInfo;
 import org.geoserver.catalog.LayerGroupInfo.Mode;
+import org.geoserver.catalog.PublishedInfo;
 import org.geoserver.ows.Dispatcher;
 import org.geoserver.platform.GeoServerExtensionsHelper;
 import org.geoserver.security.ResourceAccessManager;
@@ -37,6 +39,8 @@ public class SecureCatalogImplGroupsTest extends AbstractAuthorizationTest {
     private static final String OPAQUE_GROUP_NAME = "opaque";
 
     private static final String SINGLE_GROUP_NAME = "single";
+    
+    private static final String NESTED_GROUP_NAME = "nested";
 
     private static final String[] DEFAULT_RULES = new String[] { "*.*.r=*", "*.*.w=*" };
 
@@ -157,6 +161,7 @@ public class SecureCatalogImplGroupsTest extends AbstractAuthorizationTest {
 
     @Test
     public void testWmsSingleAndOpaqueGroup() throws Exception {
+        setupRequestThreadLocal("WMS");
         // setup groups
         LayerGroupInfo opaque = buildLayerGroup(OPAQUE_GROUP_NAME, Mode.OPAQUE_CONTAINER, null,
                 statesLayer, roadsLayer);
@@ -170,8 +175,8 @@ public class SecureCatalogImplGroupsTest extends AbstractAuthorizationTest {
         SecurityContextHolder.getContext().setAuthentication(roUser);
 
         // direct access to layers not allowed because the only container is in opaque mode
-        assertNotNull(sc.getLayerByName(statesLayer.prefixedName()));
-        assertNotNull(sc.getLayerByName(roadsLayer.prefixedName()));
+        assertNull(sc.getLayerByName(statesLayer.prefixedName()));
+        assertNull(sc.getLayerByName(roadsLayer.prefixedName()));
 
         // we can access the group and the layers through it
         LayerGroupInfo opaqueSecuredGroup = sc.getLayerGroupByName(opaque.prefixedName());
@@ -409,6 +414,214 @@ public class SecureCatalogImplGroupsTest extends AbstractAuthorizationTest {
         assertEquals(3, securedNamedGroup.layers().size());
         LayerGroupInfo securedOpaqueGroup = sc.getLayerGroupByName(OPAQUE_GROUP_NAME);
         assertEquals(2, securedOpaqueGroup.layers().size());
+    }
+    
+    /**
+     * Same as {@link #testWmsStandaloneOpaqueGroup()} but with a nested group as the testing target
+     */
+    @Test
+    public void testWmsNestedInStandaloneOpaqueGroup() throws Exception {
+        setupRequestThreadLocal("WMS");
+        // setup group
+        LayerGroupInfo nested = buildLayerGroup(NESTED_GROUP_NAME, Mode.NAMED, null, statesLayer);
+        LayerGroupInfo opaque = buildLayerGroup(OPAQUE_GROUP_NAME, Mode.OPAQUE_CONTAINER, null,
+                nested, roadsLayer);
+        layerGroups = Arrays.asList(nested, opaque);
+        populateCatalog();
+
+        // setup security
+        buildManager(DEFAULT_RULES);
+        SecurityContextHolder.getContext().setAuthentication(roUser);
+
+        // direct access to nested group not allowed, nor to its layers
+        assertNull(sc.getLayerGroupByName(NESTED_GROUP_NAME));
+        assertNull(sc.getLayerByName(statesLayer.prefixedName()));
+        assertNull(sc.getLayerByName(roadsLayer.prefixedName()));
+
+        // however we can access the group as part of the opaque one
+        LayerGroupInfo securedGroup = sc.getLayerGroupByName(opaque.prefixedName());
+        assertNotNull(securedGroup);
+        assertEquals(2, securedGroup.getLayers().size());
+        assertThat(securedGroup.getLayers(), contains(nested, roadsLayer));
+        assertThat(securedGroup.layers(), contains(statesLayer, roadsLayer));
+    }
+    
+    /**
+     * Same as {@link #testWmsNamedOpaqueGroup()} but with a nested group as the testing target
+     */
+    @Test
+    public void testWmsNestedInNamedOpaqueGroup() throws Exception {
+        setupRequestThreadLocal("WMS");
+        // setup group
+        LayerGroupInfo nested = buildLayerGroup(NESTED_GROUP_NAME, Mode.NAMED, null, statesLayer);
+        LayerGroupInfo opaque = buildLayerGroup(OPAQUE_GROUP_NAME, Mode.OPAQUE_CONTAINER, null,
+                nested, roadsLayer);
+        LayerGroupInfo named = buildLayerGroup(NAMED_GROUP_NAME, Mode.NAMED, null, nested,
+                roadsLayer);
+        layerGroups = Arrays.asList(nested, named, opaque);
+        populateCatalog();
+
+        // setup security
+        buildManager(DEFAULT_RULES);
+        SecurityContextHolder.getContext().setAuthentication(roUser);
+
+        // direct access to layers and nested group allowed because of the named group
+        assertNotNull(sc.getLayerGroupByName(nested.prefixedName()));
+        assertNotNull(sc.getLayerByName(statesLayer.prefixedName()));
+        assertNotNull(sc.getLayerByName(roadsLayer.prefixedName()));
+
+        // we can access the group and the layers through it
+        LayerGroupInfo securedOpaqueGroup = sc.getLayerGroupByName(opaque.prefixedName());
+        assertNotNull(securedOpaqueGroup);
+        assertEquals(2, securedOpaqueGroup.getLayers().size());
+        assertThat(securedOpaqueGroup.getLayers(), contains(nested, roadsLayer));
+        
+        // and same for named
+        LayerGroupInfo securedNamedGroup = sc.getLayerGroupByName(named.prefixedName());
+        assertNotNull(securedNamedGroup);
+        assertEquals(2, securedNamedGroup.getLayers().size());
+        assertThat(securedNamedGroup.getLayers(), contains(nested, roadsLayer));
+    }
+    
+    /**
+     * Same as {@link #testWmsSingleAndOpaqueGroup()} but with a nested group as the testing target
+     */
+    @Test
+    public void testWmsNestedInSingleAndOpaqueGroup() throws Exception {
+        setupRequestThreadLocal("WMS");
+        
+        // setup groups
+        LayerGroupInfo nested = buildLayerGroup(NESTED_GROUP_NAME, Mode.NAMED, null, statesLayer);
+        LayerGroupInfo opaque = buildLayerGroup(OPAQUE_GROUP_NAME, Mode.OPAQUE_CONTAINER, null,
+                nested, roadsLayer);
+        LayerGroupInfo single = buildLayerGroup(SINGLE_GROUP_NAME, Mode.SINGLE, null, nested,
+                roadsLayer);
+        layerGroups = Arrays.asList(nested, single, opaque);
+        populateCatalog();
+
+        // setup security
+        buildManager(DEFAULT_RULES);
+        SecurityContextHolder.getContext().setAuthentication(roUser);
+
+        // direct access to layers and nested group not allowed because the only container is in opaque mode
+        assertNull(sc.getLayerGroupByName(nested.prefixedName()));
+        assertNull(sc.getLayerByName(statesLayer.prefixedName()));
+        assertNull(sc.getLayerByName(roadsLayer.prefixedName()));
+
+        // we can access the group and the layers through it
+        LayerGroupInfo opaqueSecuredGroup = sc.getLayerGroupByName(opaque.prefixedName());
+        assertNotNull(opaqueSecuredGroup);
+        assertEquals(2, opaqueSecuredGroup.getLayers().size());
+        assertThat(opaqueSecuredGroup.getLayers(), contains(nested, roadsLayer));
+
+        LayerGroupInfo securedSingleGroup = sc.getLayerGroupByName(single.prefixedName());
+        assertNotNull(securedSingleGroup);
+        assertEquals(2, securedSingleGroup.getLayers().size());
+        assertThat(securedSingleGroup.getLayers(), contains(nested, roadsLayer));
+    }
+    
+    /**
+     * Same as {@link #testWmsMilitaryNamedAndPublicOpaqueGroup()} but with a nested group as the testing target
+     */
+    @Test
+    public void testWmsNestedInMilitaryNamedAndPublicOpaqueGroup() throws Exception {
+        setupRequestThreadLocal("WMS");
+        // setup groups
+        LayerGroupInfo nested = buildLayerGroup(NESTED_GROUP_NAME, Mode.NAMED, null, statesLayer);
+        LayerGroupInfo opaque = buildLayerGroup(OPAQUE_GROUP_NAME, Mode.OPAQUE_CONTAINER, null,
+                nested, roadsLayer);
+        LayerGroupInfo named = buildLayerGroup(NAMED_GROUP_NAME, Mode.NAMED, null, nested,
+                roadsLayer);
+        layerGroups = Arrays.asList(nested, opaque, named);
+        populateCatalog();
+
+        // setup security
+        buildManager(new String[] { "named.r=MILITARY" });
+
+        // try the ro user
+        SecurityContextHolder.getContext().setAuthentication(roUser);
+
+        // ... direct access to layers not allowed because the named group is not visible
+        assertNull(sc.getLayerGroupByName(nested.prefixedName()));
+        assertNull(sc.getLayerByName(statesLayer.prefixedName()));
+        assertNull(sc.getLayerByName(roadsLayer.prefixedName()));
+
+        // ... but we can access the opaque group and the layers through it
+        LayerGroupInfo securedGroup = sc.getLayerGroupByName(OPAQUE_GROUP_NAME);
+        assertNotNull(securedGroup);
+        assertEquals(2, securedGroup.getLayers().size());
+        assertThat(securedGroup.getLayers(), contains(nested, roadsLayer));
+
+        // now try with the military one
+        SecurityContextHolder.getContext().setAuthentication(milUser);
+
+        // ... direct access to layers allowed because the named group is visible to the mil user
+        assertNotNull(sc.getLayerGroupByName(nested.prefixedName()));
+        assertNotNull(sc.getLayerByName(statesLayer.prefixedName()));
+        assertNotNull(sc.getLayerByName(roadsLayer.prefixedName()));
+    }
+
+    /**
+     * Same as {@link #testWmsPublicSingleAndSecuredOpaqueGroup} but with a nested group as the testing target
+     * @throws Exception
+     */
+    @Test
+    public void testWmsNestedInPublicSingleAndSecuredOpaqueGroup() throws Exception {
+        setupRequestThreadLocal("WMS");
+        // setup groups
+        LayerGroupInfo nested = buildLayerGroup(NESTED_GROUP_NAME, Mode.NAMED, null, statesLayer);
+        LayerGroupInfo opaque = buildLayerGroup(OPAQUE_GROUP_NAME, Mode.OPAQUE_CONTAINER, null,
+                forestsLayer, nested);
+        LayerGroupInfo single = buildLayerGroup(SINGLE_GROUP_NAME, Mode.SINGLE, null, roadsLayer,
+                nested);
+        layerGroups = Arrays.asList(nested, single, opaque);
+        populateCatalog();
+
+        // setup security, single allowed to everybody, opaque to military only
+        buildManager(SINGLE_GROUP_NAME + ".r=*", OPAQUE_GROUP_NAME + ".r=MILITARY");
+        SecurityContextHolder.getContext().setAuthentication(roUser);
+
+        // try the ro user
+        SecurityContextHolder.getContext().setAuthentication(roUser);
+        
+        // ... the only directly available layer should be roads, contained in single
+        assertNotNull(sc.getLayerByName(roadsLayer.prefixedName()));
+        assertNull(sc.getLayerByName(forestsLayer.prefixedName()));
+        assertNull(sc.getLayerByName(statesLayer.prefixedName()));
+        assertNull(sc.getLayerGroupByName(nested.prefixedName()));
+
+        // ... and even via groups it's the same
+        assertNull(sc.getLayerGroupByName(opaque.prefixedName()));
+        LayerGroupInfo securedSingleGroup = sc.getLayerGroupByName(single.prefixedName());
+        assertNotNull(securedSingleGroup);
+        assertEquals(1, securedSingleGroup.getLayers().size());
+        
+        // however switching to mil user
+        SecurityContextHolder.getContext().setAuthentication(milUser);
+
+        // ... same as above for direct access
+        assertNotNull(sc.getLayerByName(roadsLayer.prefixedName()));
+        assertNull(sc.getLayerByName(forestsLayer.prefixedName()));
+        assertNull(sc.getLayerByName(statesLayer.prefixedName()));
+        assertNull(sc.getLayerGroupByName(nested.prefixedName()));
+        
+        // but the opaque group is now visible along with its layers
+        securedSingleGroup = sc.getLayerGroupByName(single.prefixedName());
+        assertNotNull(securedSingleGroup);
+        assertEquals(2, securedSingleGroup.getLayers().size());
+        assertThat(securedSingleGroup.getLayers(), contains(roadsLayer, nested));
+        LayerGroupInfo nestedInSingle = (LayerGroupInfo) securedSingleGroup.getLayers().get(1);
+        assertEquals(1, nestedInSingle.getLayers().size());
+        assertThat(nestedInSingle.getLayers(), contains(statesLayer));
+        
+        LayerGroupInfo securedOpaqueGroup = sc.getLayerGroupByName(opaque.prefixedName());
+        assertNotNull(securedOpaqueGroup);
+        assertEquals(2, securedOpaqueGroup.getLayers().size());
+        assertThat(securedOpaqueGroup.getLayers(), contains(forestsLayer, nested));
+        LayerGroupInfo nestedInOpaque = (LayerGroupInfo) securedSingleGroup.getLayers().get(1);
+        assertEquals(1, nestedInOpaque.getLayers().size());
+        assertThat(nestedInOpaque.getLayers(), contains(statesLayer));
+
     }
 
 }
